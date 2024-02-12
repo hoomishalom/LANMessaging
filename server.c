6f9b123beedeed6115e24c01acfba6882459e0bf
Tom Shabtay <tomshabtay75@gmail.com>
2024-02-12 18:19:49 +0200

    test committing

--- server.c
+++ server.c
@@ -1,9 +1,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <unistd.h>
+#include <sys/socket.h>
 
 int main(){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     return 0;
 }
