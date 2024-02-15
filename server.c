6db8a438960920741216d8962beb956cb7a7c5e3
hoomi <hoomi@debian-tom>
2024-02-15 21:43:52 +0200

    socket creation done

--- server.c
+++ server.c
@@ -3,10 +3,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(){
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+const int PORT = 5678;
+const char* ADDR = "127.0.0.1";
 
+int main()
+{
+    int option = 1;
+    int serverSock;
+    struct sockaddr_in address;
+
+    if ((serverSock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {
+        fprintf(stderr, "socket() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(serverSock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)))
+    {
+        fprintf(stderr, "setsockopt() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    if (inet_aton(ADDR, &address.sin_addr) == 0)    // sets address.sin_addr.s_addr
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(serverSock, (struct sockaddr*)&address, sizeof(address)) == -1) 
+    {
+        fprintf(stderr, "bind() failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
